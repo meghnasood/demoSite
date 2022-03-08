88c24ccb4d30845eeb51ae0d398dba2f4fafe687
meghnasood <meghnasood22@gmail.com>
2022-03-08 23:00:01 +0530

    objects

---
+++ module2/intro.js
@@ -0,0 +1,21 @@
+// let objecteg = {
+//     name: "meghna",
+//     age: 12,
+//     phone : 44
+// }
+
+//console.log(objecteg);
+
+let captainamerica = {
+    name: "steve",
+    friends: ['bucky','natasha','tony'],
+    address: {
+        state: "delhi",
+        city: "new delhi"
+    },
+    sayHi : function(){
+        console.log("hello this is function from object")
+    },
+    phone: 22
+}
+captainamerica.sayHi();
